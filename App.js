803b88f9b19d9a7bcdb8b3b97fb88baa4dd985f6
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2023-01-19 23:56:28 -0300

    06 Branch Git

--- App.js
+++ App.js
@@ -1,50 +1,75 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Alert, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
-  const regiaoInicial = {
-    // SP
-    latitude: -23.533773,
-    longitude: -46.65529,
+  /* State para localização */
+  const [minhaLocalizacao, setMinhalocalizacao] = useState(null);
+  useEffect(() => {
+    async function obterLocalizacao() {
+      /* Acessando o status da requisição de permissão de uso */
+      const { status } = Location.requestForegroundPermissionsAsync();
+      //verificando o status
 
-    latitudeDelta: 30,
-    longitudeDelta: 30,
+/*          if (status !== "granted") {
+        Alert.alert(
+          "Ops",
+          "Você não autorizou o uso de recursos de localização"
+        );
+        return;
+      }  */
+
+      //Acessando os dados de geolocalização
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+      // Adicionando os dados ao state
+      setMinhalocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+
+  const regiaoInicial = {
+    latitude: 37.401437,
+    longitude: -116.86773,
+
+    latitudeDelta: 0.0222,
+    longitudeDelta: 0.00121,
   };
 
-  const marcarLocal = (event) => {
+  const [localizacao, setLocalizacao] = useState();
+  const novaLocalizacao = (event) => {
     setLocalizacao({
-      latitudeDelta: 0.3,
-      longitudeDelta: 0.3,
-      latitude: event.nativeEvent.coordinate.latitude,
-      longitude: event.nativeEvent.coordinate.longitude,
+      latitude: minhaLocalizacao.coords.latitude,
+      longitude: minhaLocalizacao.coords.longitude,
+      latitudeDelta: 0.0222,
+      longitudeDelta: 0.00121,
     });
     console.log(localizacao);
   };
-  const [localizacao, setLocalizacao] = useState();
 
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
-        <MapView
-          style={estilos.map}
-          // initialRegion={regiaoInicial}
-          // Coalescencia (Se existir localizacao mostrar localizacao, senão região inicial)
-          region={localizacao ?? regiaoInicial}
-          liteMode={false}
-          mapType="standard"
-          onPress={marcarLocal}
-        >
-          {/* Condicional SE houver localização inicial  mostre o marcador, senão não */}
-          {localizacao && (
-            <Marker
-              coordinate={localizacao}
-              title="Aqui!!"
-              onPress={(e) => console.log(e.nativeEvent)}
-            />
-          )}
-        </MapView>
+        <View style={estilos.botao}>
+          <Button
+            title="descobrir minha localização"
+            onPress={novaLocalizacao}
+          />
+        </View>
+        <View style={estilos.viewMapa}>
+          <MapView
+            style={estilos.map}
+            // initialRegion={regiaoInicial}
+            region={localizacao ?? regiaoInicial}
+            liteMode={false}
+            mapType="standard"
+          >
+            {localizacao && (
+              <Marker coordinate={localizacao} title="Titulo" draggable />
+            )}
+          </MapView>
+        </View>
       </View>
     </>
   );
@@ -54,6 +79,8 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
   },
+  botao: {},
+  viewMapa: { flex: 1 },
   map: {
     width: "100%",
     height: "100%",
